b88903f611619c9d2f1438d493d86bfdf252634a
150381 <150381@01HW238666.TCSTRU.COM>
2012-07-28 09:56:35 +0530

    created

---
+++ src/test/resources/org/abc/framework/writer/create_target_table.sql
@@ -0,0 +1,8 @@
+create table target_table (
+	ID varchar2(1),
+	NAME varchar2(5),
+	DETAIL_INDENTIFIER varchar2(1),
+	ORDER_ID varchar2(5),
+	TRAILER_INDENTIFIER varchar2(1),
+	RECORD_COUNT varchar2(5)
+);
